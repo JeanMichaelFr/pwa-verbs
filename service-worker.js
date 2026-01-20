31791f04d0936acbf87881f2cd3c01b206df730d
JeanMichaelFr <imirobooksy@gmail.com>
2026-01-20 15:37:31 +0100

    Refactor
    update

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,36 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("verbs-cache").then(cache =>
-      cache.addAll([
-        "index.html",
-        "app.js",
-        "verben.json",
-        "style.css"
-      ])
+const CACHE_NAME = "verbs-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "index.html",
+  "app.js",
+  "verben.json",
+  "style.css",
+  "manifest.json"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
     )
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
   );
 });
